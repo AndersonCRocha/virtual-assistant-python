1ceedea49e07d9009ce9cd1d718e631052e566f8
AndersonRochaIZAP <anderson.rocha@izap.com.br>
2021-04-09 12:00:54 -0300

    Changed assistant name

--- src/init.py
+++ src/init.py
@@ -4,7 +4,7 @@ from Assistant import Assistant
 if __name__ == '__main__':
     assintant = Assistant()
 
-    print('Olá, sou o assistence virtual Ted!\n')
+    print('Olá, sou o assistence virtual Jubileu!\n')
     
     while(True):
         try:
